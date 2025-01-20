ad6927bd4905966427e98c58f434919f04a1f467
Ayuik <marcosayelen@gmail.com>
2025-01-20 15:40:39 +0100

    better. text is missing

--- src/ColorInput.jsx
+++ src/ColorInput.jsx
@@ -2,22 +2,31 @@ import React, {useState} from 'react'
 
 function ColorInput(){
     const [color, setColor] = useState('#ffffff')
+    const [text, setText] = useState ("#ffffff")
 
     const boxStyles = {
-        height: "32px",
-        width:"32px",
-        border: "solid 2px",  
-        backgroundColor: color
+        height: "200px",
+        width:"200px",
+        border: "solid 1px black",
+        borderRadius: "20%",
+        backgroundColor: color,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "white",
+        marginBottom: "10%",
     }
 
     
     function handleColorSelection(event){
     setColor(event.target.value)
+    setText(event.target.textContent)
     }
 
     return(
         <>
-            <div style={boxStyles}></div>
+            <div style={boxStyles}>Selected color:{text}</div>
+            <label>Select a color</label>
             <input type="color" value={color} onChange={(e) =>handleColorSelection(e)}></input>
         </>
     )
